9078c19c103ecbe0c3588c573179ed2e0d3f618f
aaric <vipaaric@gmail.com>
2019-03-26 17:48:18 +0800

    0.0.1-SNAPSHOT init

---
+++ src/main/java/com/cooggo/springboot2/Application.java
@@ -0,0 +1,18 @@
+package com.cooggo.springboot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot Launcher.
+ *
+ * @author Aaric, created on 2019-03-26T16:49.
+ * @since 0.0.1-SNAPSHOT
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
