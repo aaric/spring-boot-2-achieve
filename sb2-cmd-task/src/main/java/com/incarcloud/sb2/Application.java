733289413f5080ee323df0f68f5a1e3a0d95ba94
aaric <qlhan@incarcloud.com>
2019-06-21 18:36:41 +0800

    0.2.0-SNAPSHOT Define the modules

---
+++ sb2-cmd-task/src/main/java/com/incarcloud/sb2/Application.java
@@ -0,0 +1,18 @@
+package com.incarcloud.sb2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot Launcher.
+ *
+ * @author Aaric, created on 2019-06-21T18:00.
+ * @since 0.2.0-SNAPSHOT
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
