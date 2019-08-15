1f297342586ccccb8f40b338893608344ae1f22d
aaric <vipaaric@gmail.com>
2019-08-15 18:01:31 +0800

    0.8.0-SNAPSHOT Add FtpService Support

---
+++ sb2-service-common/src/main/java/com/incarcloud/common/file/FtpService.java
@@ -0,0 +1,10 @@
+package com.incarcloud.common.file;
+
+/**
+ * FTP文件服务接口
+ *
+ * @author Aaric, created on 2019-08-15T17:50.
+ * @since 0.8.0-SNAPSHOT
+ */
+public interface FtpService {
+}
---
+++ sb2-service-common/src/test/java/com/incarcloud/common/file/FtpServiceTest.java
@@ -0,0 +1,10 @@
+package com.incarcloud.common.file;
+
+/**
+ * FtpServiceTest
+ *
+ * @author Aaric, created on 2019-08-15T17:51.
+ * @since 0.8.0-SNAPSHOT
+ */
+public class FtpServiceTest {
+}
