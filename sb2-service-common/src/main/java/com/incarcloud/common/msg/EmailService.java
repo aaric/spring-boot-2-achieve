84c6e46a0cd351e0942de9cc05372129bab1877c
aaric <vipaaric@gmail.com>
2019-08-22 12:29:07 +0800

    0.8.1-SNAPSHOT Add EmailService Support

---
+++ sb2-service-common/src/main/java/com/incarcloud/common/msg/EmailService.java
@@ -0,0 +1,10 @@
+package com.incarcloud.common.msg;
+
+/**
+ * 邮件服务接口
+ *
+ * @author Aaric, created on 2019-08-22T11:46.
+ * @since 0.8.1-SNAPSHOT
+ */
+public interface EmailService {
+}
---
+++ sb2-service-common/src/main/java/com/incarcloud/common/msg/impl/EmailServiceImpl.java
@@ -0,0 +1,12 @@
+package com.incarcloud.common.msg.impl;
+
+import com.incarcloud.common.msg.EmailService;
+
+/**
+ * 邮件服务实现
+ *
+ * @author Aaric, created on 2019-08-22T11:47.
+ * @since 0.8.1-SNAPSHOT
+ */
+public class EmailServiceImpl implements EmailService {
+}
